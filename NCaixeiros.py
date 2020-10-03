da5475f807d28b7e9e45a39eb18ca30e71e8d636
João Pedro <34511773+jpkun17@users.noreply.github.com>
2020-10-03 17:14:47 -0300

    Update 0.2
    correção de erro no algoritmo, algora a ordem das cidades é calculada

--- NCaixeiros.py
+++ NCaixeiros.py
@@ -8,6 +8,36 @@ def calcula_distancia(cidade1, cidade2):
     result = math.sqrt(pow(cidade2['x'] - cidade1['x'], 2) + pow(cidade2['y'] - cidade1['y'], 2))
     return round(result, 3)
 
+def vizinho_mais_proximo(grafo):
+
+    grafo[0]['visitado'] = True
+    vertice_atual = grafo[0]
+    tamanho_grafo = len(grafo)
+    menor_distancia = calcula_distancia(vertice_atual, grafo[1])
+    resposta = []
+    resposta.append(grafo[0])
+    total = 0
+
+    while len(resposta) < tamanho_grafo:
+        i = 0
+        while i < tamanho_grafo:
+            if not grafo[i]['visitado']:
+                distancia = calcula_distancia(vertice_atual, grafo[i])
+                if distancia < menor_distancia:
+                    menor_distancia = distancia
+                    vertice_atual = grafo[i]
+                    i += 1
+            i += 1
+        resposta.append(vertice_atual)
+        vertice_atual['visitado'] = True
+        total += menor_distancia
+        menor_distancia = 9999999999
+        
+    
+    print(total)
+
+    return resposta
+
 def main(args):
     #checa se os argumentos .
     if(len(args) != 3):
@@ -28,7 +58,7 @@ def main(args):
     #coordenada y.
     nome = 0
     for linha in arqv_entrada.readlines()[slice(numero_cidades)]:
-        cidade = {'nome' : nome, 'x' : int(linha.split()[0]), 'y' : int(linha.split()[1])}
+        cidade = {'nome' : nome, 'visitado' : False, 'x' : int(linha.split()[0]), 'y' : int(linha.split()[1])}
         lista_cidades.append(cidade)
         nome += 1
     
@@ -43,9 +73,9 @@ def main(args):
     #    arqv_saida.write(str(number) + "\n")
     
     #testes
-    print(lista_cidades[0])
-    print(lista_cidades[0]['x'])
-    print(calcula_distancia(lista_cidades[0], lista_cidades[1]))
+    result = vizinho_mais_proximo(lista_cidades)
+    for cidade in result:
+        print(cidade['nome'])
     print("Concluido!")
     return 0
 
